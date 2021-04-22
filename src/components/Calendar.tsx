dbeefc4a37edd89b18cbb96f9082ca8cdf4f3082
Nishant Singh <admin@nishantsingh.org>
2021-04-22 10:16:22 +0530

    changed case for styled components

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,17 +1,17 @@
 import React from "react";
-import Styled from "styled-components";
+import styled from "styled-components";
 
 import WeekView from "./WeekView";
 
-const CalendarWrapper = Styled.section`
+const CalendarWrapper = styled.section`
   padding: 0;
   margin: 0;
   width: 100vw;
   min-height: 100vh;
-  background: #00A8F4;
+  background: #00a8f4;
   display: flex;
   justify-content: center;
-  align-items: center
+  align-items: center;
 `;
 
 const Calendar = () => {
